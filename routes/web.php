d7ca0f443c103acb3b2cb3f4d6aea708735ca4b4
Rakibul Hasan Joy <rakibulhasanjoy009@gmail.com>
2025-08-07 20:25:42 +0600

    Fix import order and clean up routes: reorder TaskController import and remove unnecessary Auth::routes() line.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use App\Http\Controllers\TaskController;
+
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,8 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Auth::routes();
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
@@ -27,5 +26,5 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
-?>
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
